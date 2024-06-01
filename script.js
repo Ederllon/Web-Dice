63c910dc7bc2d2a9a9d5fdd14f9d897511ddbbe3
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-05-31 22:31:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,5 +7,5 @@ function Action(){
     var max = parseInt(document.getElementById('max').value);
     var min = parseInt(document.getElementById('min').value);
     
-    respost.innerHTML = 'valor do dado: ' + Math.floor(Math.random() * (max - min + 1)) + min;
+    respost.innerHTML = 'valor do dado: ' + Math.floor(Math.random() * (max - min + 1)+min);
 }
