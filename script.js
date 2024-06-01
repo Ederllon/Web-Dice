ce1e04577e23fb0168fcbf4437aa8ad7a0c0ebaa
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-05-31 22:16:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 const respost = document.getElementById('respost');
-
-
-
 const btn = document.getElementById('btn');
-
 
 btn.addEventListener('click', Action)
 
 function Action(){
     var max = parseInt(document.getElementById('max').value);
     var min = parseInt(document.getElementById('min').value);
+    
     var dice = Math.floor(Math.random() * (max - min + 1)) + min;
     
-
-    
     respost.innerHTML = 'valor do dado: ' + dice;
-
-
 }
