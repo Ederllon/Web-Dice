265464b602024fd04456e6b7c009f381bfdcb938
Ederllon <156702239+Ederllon@users.noreply.github.com>
2024-06-01 23:01:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ const btn = document.getElementById('btn');
 btn.addEventListener('click', Action)
 
 function Action(){
-    var max = parseInt(document.getElementById('max').value);
-    var min = parseInt(document.getElementById('min').value);
     
+    const max = parseInt(document.getElementById('max').value);
+    const min = parseInt(document.getElementById('min').value);
+    
+    if(isNaN(max) || isNaN(min)){
+        respost.innerHTML = "Algum valor acima não foi dado!";
+    }
+    else{
     respost.innerHTML = 'valor do dado: ' + Math.floor(Math.random() * (max - min + 1)+min);
+    }
 }
